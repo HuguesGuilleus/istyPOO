4678062025420fe62c85c7893a245cb045ddadc6
hugues <ghugues@netc.fr>
2020-02-14 17:48:13 +0100

    ex3

---
+++ ex3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# GUILLEUS Hugues IATIC3 <hugues.guilleus@ens.uvsq.fr>
+
+couleur = {
+	"r": "Rouge",
+	"v": "Vert",
+	"b": "bleu",  # je ne sais pas pourquoi le bleu est en minuscule
+}
+
+inp = input("[rvb]: ").lower()
+
+if inp in couleur:
+	print(couleur[inp])
+else:
+	print("Couleur non reconnue")
