666eaa0ba6da058875d5fa5de28eb5c987e00dc8
hugues <ghugues@netc.fr>
2020-02-14 17:00:50 +0100

    Add ex1.py

---
+++ ex1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# GUILLEUS Hugues IATIC3 <hugues.guilleus@ens.uvsq.fr>
+
+print("=== Première partie")
+temps=6.892
+dist=19.7
+vit = dist / temps
+print(f"vitesse: {vit}")
+print("vitesse {0:.1f}".format(vit))
+
+print("\n=== Deuxième partie")
+nom=input("nom? ")
+age=int(input("âge? "))
+print(f"Vous êtes: {nom} et vous avez {age} ans.")
+
+# La fonction raw_input n'existe pas dans python3
