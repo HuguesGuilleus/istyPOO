631c93fd9e847b1d89a1e9cd7259c3389b37577f
hugues <ghugues@netc.fr>
2020-02-16 15:05:04 +0100

    Move in td1

--- td1/ex1.py
+++ td1/ex1.py
@@ -3,15 +3,15 @@
 # GUILLEUS Hugues IATIC3 <hugues.guilleus@ens.uvsq.fr>
 
 print("=== Première partie")
-temps=6.892
-dist=19.7
+temps = 6.892
+dist = 19.7
 vit = dist / temps
 print(f"vitesse: {vit}")
 print("vitesse {0:.1f}".format(vit))
 
 print("\n=== Deuxième partie")
-nom=input("nom? ")
-age=int(input("âge? "))
+nom = input("nom? ")
+age = int(input("âge? "))
 print(f"Vous êtes: {nom} et vous avez {age} ans.")
 
-# La fonction raw_input n'existe pas dans python3
+print("la fonction raw_input n'existe pas dans python3")
